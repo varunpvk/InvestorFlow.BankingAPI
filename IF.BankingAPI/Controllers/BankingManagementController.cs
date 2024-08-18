2db222c4343d7b1c38a396a3fb3c8b4ae0168789
varunpvk <varunpvk@gmail.com>
2024-08-18 18:46:16 +0100

    update readme

--- IF.BankingAPI/Controllers/BankingManagementController.cs
+++ IF.BankingAPI/Controllers/BankingManagementController.cs
@@ -3,37 +3,28 @@ using IF.Application.BankingService.Commands;
 using IF.Application.BankingService.Queries;
 using IF.Domain;
 using IF.Domain.DTOs;
-using IF.Domain.Enums;
 using IF.Domain.ErrorMessages;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IF.BankingAPI.Controllers
 {
-    /// <summary>
-    /// Gateway for End User Banking Operations
-    /// </summary>
     [Route("api/[controller]")]
     [ApiExplorerSettings(GroupName = "InvestorFlow Banking")]
     [ApiController]
-    public class BankingManagementController : ControllerBase
+    public class BankingController : ControllerBase
     {
         private readonly ICommandDispatcher _commandDispatcher;
-        private readonly ILogger<BankingManagementController> _logger;
+        private readonly ILogger<BankingController> _logger;
 
-        public BankingManagementController(
+        public BankingController(
             ICommandDispatcher commandDispatcher, 
-            ILogger<BankingManagementController> logger)
+            ILogger<BankingController> logger)
         {
             _commandDispatcher = commandDispatcher;
             _logger = logger;
         }
 
-        /// <summary>
-        /// Creates a Bank Account
-        /// </summary>
-        /// <param name="command"></param>
-        /// <returns></returns>
-        [HttpPost("create-account")]
+        [HttpPost("create")]
         public async Task<IActionResult> CreateBankAccountAsync([FromBody] CreateBankAccountCommand command)
         {
             _logger.LogInformation("Creating Bank Account for Customer {CustomerId}", command.CustomerId);
@@ -45,16 +36,11 @@ namespace IF.BankingAPI.Controllers
             );
         }
 
-        /// <summary>
-        /// Deletes a Bank Account
-        /// </summary>
-        /// <param name="id">Customer ID</param>
-        /// <returns></returns>
-        [HttpDelete("delete-account/{id}")]
-        public async Task<IActionResult> DeleteBankAccountAsync(Guid id)
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteBankAccountAsync([FromBody] DeleteBankAccountCommand command)
         {
-            _logger.LogInformation("Deleting Bank Account {Id}", id);
-            var command = new DeleteBankAccountCommand(id);
+            _logger.LogInformation("Deleting Bank Account {Id}", command.CustomerId);
+            //var command = new DeleteBankAccountCommand(id);
             var result = await _commandDispatcher.DispatchAsync<DeleteBankAccountCommand, Result<bool, ValidationError>>(command);
 
             return result.Match<IActionResult>(
@@ -63,19 +49,12 @@ namespace IF.BankingAPI.Controllers
             );
         }
 
-        /// <summary>
-        /// Adds money to a Bank Account
-        /// </summary>
-        /// <param name="id">Customer ID</param>
-        /// <param name="amount"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        [HttpPut("{id}/add-money/{amount}/type/{type}")]
-        public async Task<IActionResult> AddMoneyToAccountAsync(Guid id, decimal amount, string type)
+        [HttpPut("add-money")]
+        public async Task<IActionResult> AddMoneyToAccountAsync([FromBody] AddMoneyCommand command)
         {
-            _logger.LogInformation("Adding {Amount} to Account {Id}", amount, id);
-            var accountType = (AccountType) Enum.Parse(typeof(AccountType), type, true); 
-            var command = new AddMoneyCommand(id, amount, "GBP", accountType);
+            _logger.LogInformation("Adding {Amount} to Account {Id}", command.Amount, command.CustomerId);
+            //var accountType = (AccountType) Enum.Parse(typeof(AccountType), command.Type, true); 
+            //var command = new AddMoneyCommand(id, amount, "GBP", accountType);
             var result = await _commandDispatcher.DispatchAsync<AddMoneyCommand, Result<bool, ValidationError>>(command);
 
             return result.Match<IActionResult>(
@@ -84,19 +63,12 @@ namespace IF.BankingAPI.Controllers
             );
         }
 
-        /// <summary>
-        /// Withdraws money from a Bank Account
-        /// </summary>
-        /// <param name="id">Customer ID</param>
-        /// <param name="amount"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        [HttpPut("{id}/withdraw-money/{amount}/type/{type}")]
-        public async Task<IActionResult> WithdrawMoneyFromAccountAsync(Guid id, decimal amount, string type)
+        [HttpPut("withdraw-money")]
+        public async Task<IActionResult> WithdrawMoneyFromAccountAsync([FromBody] WithdrawMoneyCommand command)
         {
-            _logger.LogInformation("Withdrawing {Amount} from Account {Id}", amount, id);
-            var accountType = (AccountType)Enum.Parse(typeof(AccountType), type, true);
-            var command = new WithdrawMoneyCommand(id, amount, "GBP", accountType);
+            _logger.LogInformation("Withdrawing {Amount} from Account {Id}", command.Amount, command.CustomerId);
+            //var accountType = (AccountType)Enum.Parse(typeof(AccountType), type, true);
+            //var command = new WithdrawMoneyCommand(id, amount, "GBP", accountType);
             var result = await _commandDispatcher.DispatchAsync<WithdrawMoneyCommand, Result<bool, ValidationError>>(command);
 
             return result.Match<IActionResult>(
@@ -105,20 +77,12 @@ namespace IF.BankingAPI.Controllers
             );
         }
 
-        /// <summary>
-        /// Transfers funds from one bank account to another
-        /// </summary>
-        /// <param name="id">Customer ID</param>
-        /// <param name="accountId">Destination Account</param>
-        /// <param name="amount"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        [HttpPut("{id}/transfer-to/{accountId}/amount/{amount}/type/{type}")]
-        public async Task<IActionResult> TransferFundsToAccount(Guid id, Guid accountId, decimal amount, string type)
+        [HttpPut("transfer-funds")]
+        public async Task<IActionResult> TransferFundsToAccount([FromBody] TransferFundsCommand command)
         {
-            _logger.LogInformation("Transferring {Amount} from Account {Id} to Account {AccountId}", amount, id, accountId);
-            var accountType = (AccountType)Enum.Parse(typeof(AccountType), type, true);
-            var command = new TransferFundsCommand(id, accountType, accountId, amount, "GBP");
+            _logger.LogInformation("Transferring {Amount} from Account {Id} to Account {AccountId}", command.Amount, command.CustomerId, command.DestinationAccountId);
+            //var accountType = (AccountType)Enum.Parse(typeof(AccountType), type, true);
+            //var command = new TransferFundsCommand(id, accountType, accountId, amount, "GBP");
             var result = await _commandDispatcher.DispatchAsync<TransferFundsCommand, Result<bool, ValidationError>>(command);
 
             return result.Match<IActionResult>(
@@ -127,16 +91,11 @@ namespace IF.BankingAPI.Controllers
             );
         }
 
-        /// <summary>
-        /// Gets the transaction history for a bank account
-        /// </summary>
-        /// <param name="id">Customer ID</param>
-        /// <returns></returns>
-        [HttpGet("tranasctions/{id}")]
-        public async Task<IActionResult> GetTransactionHistoryAsync(Guid id)
+        [HttpGet("transaction-history")]
+        public async Task<IActionResult> GetTransactionHistoryAsync([FromBody] GetTransactionHistoryQuery query)
         {
-            _logger.LogInformation("Getting Transaction History for Account {Id}", id);
-            var query = new GetTransactionHistoryQuery(id);
+            _logger.LogInformation("Getting Transaction History for Account {Id}", query.CustomerId);
+            //var query = new GetTransactionHistoryQuery(id);
             var result = await _commandDispatcher.QueryAsync<GetTransactionHistoryQuery, Result<IDictionary<Guid, IList<TransactionDTO>>, NotFoundError>>(query);
 
             return result.Match<IActionResult>(
